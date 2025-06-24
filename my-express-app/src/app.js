9a4716645ac87968040b29ac21e5f854a5d68e2f
NajeebUrRehmanRaja <najeebraja22.2@gmail.com>
2025-06-24 17:05:30 +0500

    Enhance welcome message and implement global error handling in app.js

--- my-express-app/src/app.js
+++ my-express-app/src/app.js
@@ -1,10 +1,17 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
 // Routes
 app.get("/", (req, res, next)=>{
-    res.json({ msg: "Welcome to the Website!"});
+    res.json({ msg: "Welcome to the Website! Backend Practice."});
 })
 
+// global error handling
+
+app.use((err, req, res, next)=>{
+    const statusCode = err.statusCode || 500;
+
+    return res.status(statusCode).json({ msg : "Server Error" , errorStack : config.env === 'development' ? err.Stack : ""})
+})
 export default app;
