de63acd3c4bfcfd95258b3f174a1447c0a52cd0e
NajeebUrRehmanRaja <najeebraja22.2@gmail.com>
2025-06-24 16:22:57 +0500

    Add environment configuration: implement dotenv setup and export config for server port

--- my-express-app/server.js
+++ my-express-app/server.js
@@ -1,7 +1,8 @@
+import { config } from "./src/config/env.config.js";
 import app from "./src/app.js";
 
 const startServer = () => {
-  const port = process.env.PORT || 3000;
+  const port = config.port || 3000;
 
   app.listen(port, ()=>{
     console.log(`Listening on Port: ${port}`);
